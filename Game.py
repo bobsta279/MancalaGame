f9a0db30ff9bf503cd46334410685d3eea7404b9
bobsta279 <abenser@ucsd.edu>
2021-04-01 16:28:21 -0700

    Basic game is working

--- Game.py
+++ Game.py
@@ -3,46 +3,86 @@ class Game:
     playerTurn = 1
     BOARD_SIZE = 14
 
-    def setBoard(self, newBoard):
-        self.board = newBoard
-
+    def __init__(self, board):
+        self.board = board
+        self.playerTurn = 1
+        
     def playerOneTurn(self, chosenSlot):
-        if playerTurn != 1:
+        if self.playerTurn != 1:
             print("Not player 1\'s turn")
             return
-        if(advanceSlot(chosenSlot) is False):
+        if(chosenSlot < 0 or chosenSlot > 5 or self.board[chosenSlot] == 0):
+            print("Invalid slot chosen. Retry")
+            return
+        if(self.advanceSlot(chosenSlot) is False):
             print("player 1 turn ended")
-            playerTurn = 2
+            self.playerTurn = 2
         else:
             print("player 1 earned another turn")
 
-    def playerOneTurn(self, chosenSlot):
-        if playerTurn != 2:
+    def playerTwoTurn(self, chosenSlot):
+        if self.playerTurn != 2:
             print("Not player 2\'s turn")
             return
-        if(advanceSlot(chosenSlot) is False):
+        if(chosenSlot < 7 or chosenSlot > 12 or self.board[chosenSlot] == 0):
+            print("Invalid slot chosen. Retry")
+            return
+        if(self.advanceSlot(chosenSlot) is False):
             print("player 2 turn ended")
-            playerTurn = 1
+            self.playerTurn = 1
         else:
             print("player 2 earned another turn")
 
     def advanceSlot(self, slotNum):
-        stonesInHand = board[slotNum]
-        board[slotNum] = 0
+        stonesInHand = self.board[slotNum]
+        self.board[slotNum] = 0
         counter = 1
         while stonesInHand > 0:
-            if playerTurn = 1:
-                if (slotNum + counter) % BOARD_SIZE != 13:
+            if self.playerTurn == 1:
+                if (slotNum + counter) % self.BOARD_SIZE != 13:
                     stonesInHand -= 1
-                    board[(slotNum + counter) % BOARD_SIZE] += 1
-            elif playerTurn = 2:
-                if (slotNum + counter) % BOARD_SIZE != 6:
+                    self.board[(slotNum + counter) % self.BOARD_SIZE] += 1
+            elif self.playerTurn == 2:
+                if (slotNum + counter) % self.BOARD_SIZE != 6:
                     stonesInHand -= 1
-                    board[(slotNum + counter) % BOARD_SIZE] += 1
+                    self.board[(slotNum + counter) % self.BOARD_SIZE] += 1
 
             counter += 1
+        
+        lastSlot = (slotNum + counter - 1) % self.BOARD_SIZE
+        # If the last stone landed in an empty spot on your side
+        if self.board[lastSlot] == 1:
+            if lastSlot < 6 and self.playerTurn == 1:
+                print("player one stole from player two!")
+                slotToStealFrom = lastSlot + (2 * (6 - lastSlot))
+                self.board[6] += self.board[lastSlot] + self.board[slotToStealFrom]
+                print("They stole " + str(self.board[slotToStealFrom]) + " pebbles")
+                self.board[lastSlot] = 0
+                self.board[slotToStealFrom] = 0
+            if lastSlot > 7 and lastSlot < 13 and self.playerTurn == 2:
+                print("player two stole from player one!")
+                slotToStealFrom = lastSlot - (2 * (lastSlot - 6))
+                self.board[13] += self.board[lastSlot] + self.board[slotToStealFrom]
+                print("They stole " + str(self.board[slotToStealFrom]) + " pebbles")
+                self.board[lastSlot] = 0
+                self.board[slotToStealFrom] = 0
+        
+            
+        # If the last stone landed in the your endzone
+        if lastSlot == 6 and self.playerTurn == 1:
+            return True
+        elif lastSlot == 13 and self.playerTurn == 2:
+            return True
+        return False
 
     def getBoard(self):
-        return board
+        return self.board
 
-    
+    def printBoard(self):
+        print(self.board)
+        
+    def isGameOver(self):
+        print(sum(self.board[0:6]))
+        if sum(self.board[0:6]) == 0 or sum(self.board[7:14]) == 0:
+            return True
+        return False
--- Main.py
+++ Main.py
@@ -1,2 +1,53 @@
+from Game import Game
+
 def main():
+    board = []
+    # for i in range(14):
+        # print()
+        # numPebbles = int(input("What is in spot " + str(i) + " on the board?\n"))
+        # board.append(numPebbles)
+    board = [3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3, 0]
+        
+    game = Game(board)
+    game.printBoard()
     
+    while not game.isGameOver():
+        while game.playerTurn == 1:
+            userInput = input("What spot does player one choose?\n")
+            if userInput == '':
+                print("Boi")
+                continue
+            chosenSlot = int(userInput)
+            if(chosenSlot < 0 or chosenSlot > 6):
+                print("Invalid slot chosen. Retry")
+            game.playerOneTurn(chosenSlot)
+            game.printBoard()
+        
+        if game.isGameOver():
+            break
+        
+        while game.playerTurn == 2:
+            userInput = input("What spot does player two choose?\n")
+            if userInput == '':
+                print("Boi")
+                continue
+            chosenSlot = int(userInput)
+            if(chosenSlot < 7 or chosenSlot > 12):
+                print("Invalid slot chosen. Retry")
+            game.playerTwoTurn(chosenSlot)
+            game.printBoard()
+            
+    finalBoard = game.getBoard()
+    if finalBoard[:7] > finalBoard[7:]:
+        print("Player 1 WINS")
+        print("Player 2 LOSES")
+    elif finalBoard[:7] < finalBoard[7:]:
+        print("Players 2 WINS")
+        print("Player 1 LOSES")
+    else:
+        print("It\'s a Tie")  
+        
+    print("Thanks for playing!")
+    
+if __name__ == "__main__":
+    main()
